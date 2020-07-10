f6053e72e103d5ed54fe285b248c4b9980d5a62e
BayazidMahmud17 <67729881+BayazidMahmud17@users.noreply.github.com>
2020-07-10 22:34:46 +0600

    Add files via upload

---
+++ project_jul10c.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int id;
+    cout<<"Enter your id:";
+    cin>>id;
+    if(id%2==0)
+    cout<<"\nIt's an Even number";
+    else
+    cout<<"\nIt's an Odd number";
+    
+return 0;
+}
